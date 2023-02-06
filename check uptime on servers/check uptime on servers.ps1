da9d7d0d4376a1ee56896d2673ea5db937199d3e
PoojaSKulkarni <PoojaSKulkarni@WIN-2ANFOQMEUFA>
2023-02-06 16:41:21 +0530

    Add project files.

---
+++ check uptime on servers/check uptime on servers.ps1
@@ -0,0 +1,5 @@
+﻿#
+# Script.ps1
+#
+Get-WmiObject Win32_OperatingSystem -ComputerName VM400027730,VM400018400 |
+select csname, @{LABEL='LastBootupTime' ;EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
---
+++ checking service running status on servers/checking service running status on servers.ps1
@@ -0,0 +1,6 @@
+﻿#
+# Script.ps1
+#
+Get-Service -ComputerName (Get-content C:\Users\c78494A\Desktop\server_names.txt) | Select-Object -
+Property Machinename, Name, status,  StartType | where-object {$_.Name -eq "KNTCMA_Primary"} |  Format-Table  -auto >> C
+:\Users\c78494A\Desktop\result.txt
